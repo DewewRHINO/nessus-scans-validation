1a85a03ca688a6e897725048fe3f9f519ac54ce9
DewewRHINO <jrjayme@cpp.edu>
2024-03-20 19:33:09 -0700

    Created parsed output for output of API call to scans

--- get_scans.py
+++ get_scans.py
@@ -3,6 +3,7 @@ import json
 import time
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 # Loading up environment variables, you will have to do this yourself.
 load_dotenv()
@@ -25,7 +26,7 @@ token = response.json()["token"]
 time.sleep(3)
 
 # Second request to get the scans using the token for authentication.
-url = f"https://{IP_ENDPOINT}/scans"
+url = f"https://{IP_ENDPOINT}/scans/"
 
 # Add the token to the request headers for authentication, this is the token we got earlier. This is us interacting with the session we created earlier.
 headers = {
@@ -37,6 +38,25 @@ response = requests.get(url, headers=headers, verify=False)
 
 # Check if the response was successful
 if response.status_code == 200:
-    print(response.json())
+    
+    # Pull out the necessary information
+    scans = response.json()["scans"][0]
+    last_modification_date = scans["last_modification_date"]
+    creation_date = scans["creation_date"]
+    status = scans["status"]
+    owner = scans["owner"]
+    rrules = scans["rrules"]
+    name = scans["name"]
+
+    parsed_output = f"""
+        This is the last modification date: {last_modification_date}
+        This is the creation date: {creation_date}
+        This is the status of the scan: {status}
+        This is the owner of the scan: {owner}
+        This is the rules for the scan: {rrules}
+        This is the name of the scan: {name}
+    """
+    
+    print(parsed_output)
 else:
     print(f"Failed to get scans. Status code: {response.status_code}, Message: {response.text}")
